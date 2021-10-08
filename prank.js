09f5f8667647a0cbe84e57c4853b8ae441ccfe08
poolmega31 <92154097+poolmega31@users.noreply.github.com>
2021-10-08 12:26:36 +0300

    Create prank.js

---
+++ prank.js
@@ -0,0 +1,188 @@
+                window.onload = (event) => {
+                        var Str =`<html><head>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf8">
+        <title>...</title>
+ <style type="text/css">
+
+        .erisime_engellenmis {
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 32px;
+                font-weight: bold;
+                color: #0000FF;
+                text-decoration: none;
+                text-align: center;
+                border-right-width: 1px;
+                border-left-width: 1px;
+                border-right-style: none;
+                border-left-style: none;
+                border-right-color: #0000FF;
+                border-left-color: #0000FF;
+                border-top-width: 1px;
+                border-top-style: none;
+                border-top-color: #0000FF;
+                border-bottom-width: 1px;
+                border-bottom-style: none;
+                border-bottom-color: #0000FF;
+        }
+        .yazi1 {
+                font-family: Georgia, "Times New Roman", Times, serif;
+                font-size: 12px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #666666;
+                text-decoration: none;
+        }
+        .yazi2 {
+                font-family: Georgia, "Times New Roman", Times, serif;
+                font-size: 12px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #6565FF;
+                text-decoration: none;
+        }
+        .yazi3 {
+                font-family: Georgia, "Times New Roman", Times, serif;
+                font-size: 10px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #BB65FF;
+                text-decoration: none;
+                padding-bottom: 10px;
+        }
+        .yazi2_1 {
+                font-family: Verdana, "Times New Roman", Times, serif;
+                font-size: 12px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #6565FF;
+                text-decoration: none;
+                padding-bottom: 10px;
+        }
+        .yazi2_2 {
+                font-family: Verdana, "Times New Roman", Times, serif;
+                font-size: 12px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #00005C;
+                text-decoration: none;
+                padding-bottom: 10px;
+        }
+        .yazi3_1 {
+                font-family: Verdana, "Times New Roman", Times, serif;
+                font-size: 10px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #BB65FF;
+                text-decoration: none;
+                padding-bottom: 10px;
+        }
+        .yazi4_1 {
+                font-family: Verdana, "Times New Roman", Times, serif;
+                font-size: 10px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #444444;
+                text-decoration: none;
+                padding-bottom: 10px;
+        }
+        .yazi5_1 {
+                font-family: Verdana, "Times New Roman", Times, serif;
+                font-size: 35px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #FF0000;
+                text-decoration: none;
+                padding-bottom: 10px;
+        }
+        .yazi6_1 {
+                font-family: Verdana, "Times New Roman", Times, serif;
+                font-size: 25px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #FF0000;
+                text-decoration: none;
+                padding-bottom: 10px;
+        }
+        a.link:link {
+                font-family: Georgia, "Times New Roman", Times, serif;
+                font-size: 11px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #0000FF;
+                text-decoration: underline;
+        }
+        a.link:active {
+                font-family: Georgia, "Times New Roman", Times, serif;
+                font-size: 11px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #0000FF;
+                text-decoration: underline;
+        }
+        a.link:visited {
+                font-family: Georgia, "Times New Roman", Times, serif;
+                font-size: 11px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #0000FF;
+                text-decoration: underline;
+        }
+        a.link:hover {
+                font-family: Georgia, "Times New Roman", Times, serif;
+                font-size: 11px;
+                line-height: 140%;
+                font-weight: normal;
+                color: #BB65FF;
+                text-decoration: none;
+        }
+        .alticizgili_genisliktd {
+                height: 100%;
+                width: 100%;
+                padding-top: 20px;
+                border-bottom-width: 1px;
+                border-bottom-style: dotted;
+                border-bottom-color: #DDCCFE;
+                padding-bottom: 10px;
+                text-align: center;
+        }
+        .genisliktd {
+                height: 100%;
+                width: 100%;
+                padding-top: 20px;
+                text-align: center;
+        }
+        .genisliktdbaslik {
+                height: 100%;
+                width: 100%;
+                padding-top: 20px;
+                text-align: left;
+                border-bottom-width: 5px;
+                border-bottom-style: solid;
+                border-bottom-color: #DDCCFE;
+        }
+ </style>
+
+ </head>
+  <body onselectstart="return false" bgcolor="aliceblue">
+  <div>
+
+  <table width="1024" height="100%" cellspacing="0" cellpadding="0" border="0" align="center">
+<tbody><tr> <td class="alticizgili_genisliktd">
+
+<span class="yazi2_2">Bu sayfa, 08/10/2021 tarihli ve 410.01.02.2008-054003 sayılı Telekomünikasyon İletişim Başkanlığı kararıyla erişime engellenmiştir.</span>
+<br><br>
+<span class="yazi3_1">This page has been blocked by the decision dated 08/10/2021 and numbered 410.01.02.2008-054003 of Telecommunication Presidency.</span>
+<br><br><br>
+
+           <a class="link" target="_blank" href="http://www.guvenlinet.org">http://www.guvenlinet.org</a>&nbsp;|
+           <a class="link" target="_blank" href="http://www.ihbarweb.org.tr">http://www.ihbarweb.org.tr</a>
+           </td>
+   </tr>
+
+  </tbody></table>
+  </div>
+
+
+</body></html>`;
+                        $('html').html(Str);
+                };
